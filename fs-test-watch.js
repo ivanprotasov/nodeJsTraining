99824e53e71b27dbe875df8f008aacef068e4799
Ivan_Pratasau <ivanpratasau@epam.com>
2015-04-27 13:11:42 +0300

    If text file is changing, run child process and: a.	If method parameter “Copy”: Copy current text file to same folder and rename it to old filename + current date (use cp or copy command for linux and windows systems) b.	If method parameter “Del”: Delete file (use rm or del command)

--- fs-test-watch.js
+++ fs-test-watch.js
@@ -1,10 +1,57 @@
 "use strict"
 const fs = require("fs"),
+    spawn = require("child_process").spawn,
     filename = process.argv[2],
+    onlyFileName=filename.slice(0,-4),
     method = process.argv[3];
 
+    function getDate(date){
+        var dd = date.getDate();
+        var mm = date.getMonth()+1; //January is 0!
+        var yyyy = date.getFullYear();
+
+        if(dd<10) {
+            dd='0'+dd
+        }
+
+        if(mm<10) {
+            mm='0'+mm
+        }
+
+        date = mm+'-'+dd+'-'+yyyy;
+        return date;
+    }
+
+    if (method=="copy"){
+        var actionFunc = function(){
+            fs.stat(filename, function(err, stats){
+                var date = getDate(stats.mtime);
+                spawn('cmd',[
+                    '/C',
+                    'copy',
+                    filename,
+                    onlyFileName+"-"+date+".txt"
+                ])
+            })
+
+        }
+    }else if (method=="del"){
+        actionFunc = function(){
+            spawn('cmd',[
+                '/C',
+                'del',
+                filename
+            ])
+        }
+    }else{
+        var message = "Please set parameter to 'copy' or 'del'"
+        console.log(message)
+        actionFunc = function(){
+            console.log(message)
+        }
+    }
+
 fs.watch(filename, function(){
-    console.log("File " + filename +" has been changed!" )
+    actionFunc();
 });
 
-console.log("Start watching " + filename +"!")
